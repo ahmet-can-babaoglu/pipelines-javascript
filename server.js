fa0da48275a2a81e438b2f46cfa880212add99ad
ahmet-can-babaoglu <86446839+ahmet-can-babaoglu@users.noreply.github.com>
2021-07-06 11:54:22 -0700

    add more text

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Node.js Sample!\n');
+  res.send('Hello Node.js Sample! This is a good day!\n');
 });
 
 var port = process.env.PORT||PORT;
